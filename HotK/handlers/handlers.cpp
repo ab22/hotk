40287280a1cc40d7e7c446abbe1fd1f416de6ea4
Abelardo E. Mendoza <abelardo22.9@gmail.com>
2018-06-03 16:44:25 -0600

    use namespace aliases

--- HotK/handlers/handlers.cpp
+++ HotK/handlers/handlers.cpp
@@ -1,8 +1,28 @@
 #include "handlers.h"
 
-using namespace hotk::handlers;
+namespace handlers = hotk::handlers;
 
-std::string machine_name;
+using handlers::TcpClient;
+using handlers::MessageType;
+using handlers::Win32Error;
+using handlers::Graphics;
+
+
+void handlers::process_message(TcpClient& tcp_client, const MessageType msg_type, std::vector<std::byte>&& data)
+{
+	switch (msg_type) {
+	case MessageType::MachineInfo:
+		hotk::handlers::get_machine_info(tcp_client);
+		break;
+
+	case MessageType::ScreenCapture:
+		hotk::handlers::capture_screen(tcp_client);
+		break;
+
+	default:
+		std::cout << "Unrecognized message type received: " << (uint16_t)msg_type << "\n";
+	}
+}
 
 std::string get_computer_name()
 {
@@ -20,28 +40,31 @@ std::string get_computer_name()
 	return fqdn;
 }
 
-void hotk::handlers::get_machine_info(TcpClient& tcp_client)
+void handlers::get_machine_info(TcpClient& tcp_client)
 {
-	if (machine_name.length() == 0) {
-		std::cout << "Getting machine information...\n";
+	std::string machine_name;
+	std::cout << "Getting machine information...\n";
 
-		try {
-			machine_name = get_computer_name();
-		}
-		catch (const Win32Error& err) {
-			std::cout << "Error getting computer name: "
-				<< "    code: " << err.code()
-				<< " message: " << err.what();
-
+	try {
+		machine_name = get_computer_name();
+	}
+	catch (const Win32Error& err) {
+		std::cout << "Error getting computer name: "
+			<< "    code: " << err.code()
+			<< " message: " << err.what();
 			return;
-		}
 	}
 
 	std::cout << "Sending machine name...\n";
-	tcp_client.write(MessageType::MachineInfo, machine_name.data(), machine_name.size());
+
+	std::vector<std::byte> buffer(
+		(std::byte *)machine_name.data(),
+		(std::byte *)(machine_name.data() + machine_name.size())
+	);
+	tcp_client.write(MessageType::MachineInfo, std::move(buffer));
 }
 
-void hotk::handlers::capture_screen(TcpClient& tcp_client)
+void handlers::capture_screen(TcpClient& tcp_client)
 {
 	std::cout << "Initializing graphics module...\n";
 	Graphics g;
@@ -53,20 +76,4 @@ void hotk::handlers::capture_screen(TcpClient& tcp_client)
 	auto bitmap = g.to_vector(screen_hbitmap.get());
 
 	tcp_client.write(MessageType::ScreenCapture, std::move(bitmap));
-}
-
-void hotk::handlers::process_message(TcpClient& tcp_client, const MessageType msg_type, std::vector<std::byte>&& data)
-{
-	switch (msg_type) {
-	case MessageType::MachineInfo:
-		hotk::handlers::get_machine_info(tcp_client);
-		break;
-
-	case MessageType::ScreenCapture:
-		hotk::handlers::capture_screen(tcp_client);
-		break;
-
-	default:
-		std::cout << "Unrecognized message type received: " << (uint16_t)msg_type << "\n";
-	}
 }
